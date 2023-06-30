c073ac470a4ae1af75df188a723926181e56f9a9
unusualuser1 <cshater1234@gmail.com>
2023-06-30 09:25:15 +0000

    added components file for home page

---
+++ app/components/SideBarDX.tsx
@@ -0,0 +1,7 @@
+export default function SideBarDX(){
+    return(
+        <>
+            <div id="sidebar" className="relative h-screen w-1/6 bottom-32 right-0 top-0 pt-32 pr-3 bg-white text-cyan-800"></div>
+        </>
+    )
+}
