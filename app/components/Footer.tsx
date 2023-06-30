8ad61bde9b90bd03f40264a859cb33fa365fe68b
unusualuser1 <cshater1234@gmail.com>
2023-06-30 09:30:35 +0000

    somthing

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return (
-        <>
-            <div className="bottom-0 left-0 right-0 absolute h-28 w-full bg-green-500 px-6 py-8 justify-center"> 
-                <div><img className="w-12 h-12" src="../logo.png" alt="Instagram"></img></div>
-            </div>
-        </>
-    )
-}  
