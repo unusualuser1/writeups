f08f0ce4b730725340c18c9d6b97d292c3b49548
unusualuser1 <cshater1234@gmail.com>
2023-06-30 11:58:08 +0000

    styling home page

---
+++ app/components/Difficulty.tsx
@@ -0,0 +1,11 @@
+
+
+export default function DiffPreview(){
+    return(
+        
+        <div className="w-[300px] h-full justify-center items-center pt-16 pb-32 rounded-xl bg-white m-2">
+
+        </div>
+        
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import MainContent from "./components/MainContent"
+import DiffPreview from "./components/Difficulty"
 
 export default function Home() {
   return (
     <main>
       <Header/>
-      <MainContent/>
+
+
+      <div className="flex flex-wrap w-full h-screen  pt-20 pb-36 bg-slate-500 justify-center items-center ">
+        <DiffPreview/>
+        <DiffPreview/>
+        <DiffPreview/>
+        <DiffPreview/>
+      </div>
+      
+
       <Footer/>
+
+      
     </main>
   )
 }
