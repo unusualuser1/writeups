59864a20533a2157e043b05e1d062ed6c7d6ce48
unusualuser1 <cshater1234@gmail.com>
2023-07-01 00:30:08 +0000

    Progress

--- components/Difficulty.tsx
+++ components/Difficulty.tsx
@@ -3,7 +3,7 @@
 export default function DiffPreview(){
     return(
         
-        <div className="w-[300px] h-[300px] justify-center items-center rounded-xl bg-white m-2">
+        <div className="w-[20vw] h-[300px] justify-center items-center rounded-xl bg-white m-2">
 
         </div>
         
