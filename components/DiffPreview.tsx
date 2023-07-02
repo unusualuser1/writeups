700e01db0d2b72ccc474ea1251306115ddfb6973
unusualuser1 <cshater1234@gmail.com>
2023-07-02 18:32:52 +0000

    idk

--- components/DiffPreview.tsx
+++ components/DiffPreview.tsx
@@ -20,19 +20,13 @@ export default function DiffPreview ( {difficulty} : any){
         x = '#800909';
     else
         x = '#8a8880';
-    
     return(
-        <>
         <Link href={`/${name}`}>
             <div className=" w-[250px] h-[300px] justify-center items-center rounded-xl bg-slate-300 m-2">
-                <img className="w-full h-3/4 rounded-lg border-4 bg-[../HTB_logo.png]"
-                src="../HTB_logo.png"
-                style={{borderColor:x}}
-                ></img>
+                <img className="w-full h-3/4 rounded-lg border-4 bg-[../HTB_logo.png]" style={{borderColor:x}} src="../HTB_logo.png"></img>
                 <h1 className=" pt-6 pb-6 text-black text-center align-text-bottom">{name}</h1>
             </div>
         </Link>
-        </>
         
         
         
