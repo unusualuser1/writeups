d6273575c788e6b5cde3589306703969bdcc5f39
unusualuser1 <cshater1234@gmail.com>
2023-07-03 15:12:21 +0000

    boxes

--- app/[dif]/page.tsx
+++ app/[dif]/page.tsx
@@ -18,27 +18,28 @@ export default async function difficulty({params} : any){
     let content = null;
     let response = await fetchRepoFiles('Wanasgheo','Writeups', gitToken, `/HackTheBox/${dif}?ref=main`);
     console.log(response);
-  
     return (
-        <div className=" w-[full] h-[400px] min-w-[300px] bg-slate-500 translate-y-28 py-8 px-8 rounded-lg">
-            {response?.map((resp : File) => {
-              return(
-                <Link href={`/${dif}/${resp.name}`} className="w-1/2 h-1/4" >
+      <div className=" w-[full] h-[400px] min-w-[300px] bg-slate-500 translate-y-28 py-8 px-8 rounded-lg m-4">
+          {response?.map((resp : File) => {
+            return(
+              <Link href={`/${dif}/${resp.name}`}>
 
-                  <div className=" inline-flex
-                  bg-orange-500 
-                  rounded-lg ">
-                      <div className="h-full w-ful">
-                        <img className=" h-full" src="../../HTB_logo.png"  alt="" />
-                      </div>
-                      
-                      <div className="flex w-full justify-center items-center "> 
-                        <center><h2 className=" align-middle">{resp.name}</h2></center>
-                      </div>
-                  </div>
-                </Link>
-              );
-            })}
-        </div>
-    );
+                <div className="inline-flex w-[30vw] h-1/4 m-2
+                bg-white 
+                rounded-lg 
+                min-w-[300px]">
+                    <div className=" object-contain aspect-[3/2] w-full">
+                      <img className=" rounded-l-lg h-full" src="../../HTB_logo.png"  alt="" />
+                    </div>
+                    
+                    <div className="flex w-full justify-center items-center "> 
+                      <center><h2 className="text-black align-middle">{resp.name}</h2></center>
+                    </div>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+  );
+    
 }
