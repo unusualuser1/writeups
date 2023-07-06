6a31c90190464e2559cbc313fe469cefb157a53a
unusualuser1 <cshater1234@gmail.com>
2023-07-06 12:28:02 +0000

    homepage

--- components/DiffPreview.tsx
+++ components/DiffPreview.tsx
@@ -8,15 +8,15 @@ export default function DiffPreview ( {difficulty} : any){
      if (name ==='Easy')
          x = '#4cde2f';
      else if(name === 'Medium')
-         x = '#c47621';
+         x = '#ffa500';
      else if(name == 'Hard')
-         x = '#800909';
+         x = '#ff0000';
      else
          x = '#8a8880';
      return(
          <Link href={`/HTB/${name}`} className="m-1">
              <div className=" w-[250px] h-[300px] justify-center items-center rounded-xl bg-slate-300 m-2">
-                 <img className="w-full h-3/4 rounded-lg border-4 bg-[../HTB_logo.png]" style={{borderColor:x}} src="../HTB_logo.png"></img>
+                 <img className="w-full h-3/4 rounded-lg border-[5px] bg-[../HTB_logo.png]" style={{borderColor:x}} src="../HTB_logo.png"></img>
                  <h1 className=" pt-6 pb-6 text-black text-center align-text-bottom">{name}</h1>
              </div>
          </Link>
