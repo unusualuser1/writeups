445ebfd7ba8fada17828136136d50f9afc761f1f
unusualuser1 <cshater1234@gmail.com>
2023-07-25 13:10:11 +0000

    added content in the Learn Page, added a Page in HackTools we we can store our content like a wiki, find an error in HomePage (page.tsx /app). I think the problem is we are using it either as server page and client page, the solution could be move all the server side code in a function and call in the page that will be only client

---
+++ app/Learn/HackTools/page.tsx
@@ -0,0 +1,96 @@
+export default function HackTools_Page(){
+    return(
+        <main>
+            <div id="GoBuster">
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+
+
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+                gobuster<br/>
+
+            </div>
+            <div id="wappalizer">
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+
+
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+                wappalizer<br/>
+
+            </div>
+            <div id="whatweb">
+                whatweb<br/>
+            </div>
+            <div id="nmap">
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                nmap<br/>
+                
+            </div>
+        </main>
+    )
+}
