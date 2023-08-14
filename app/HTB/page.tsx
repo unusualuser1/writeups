729adf032eb68ffe9fb4fa32688fa00a31709428
unusualuser1 <cshater1234@gmail.com>
2023-08-14 18:42:16 +0000

    changed HTB

--- app/HTB/page.tsx
+++ app/HTB/page.tsx
@@ -1,31 +1,42 @@
+
 import { PageWrapper } from "@/components/PageWrapper";
-import { getDirectoryData } from "@/lib/apiUtils";
+import { getDirectoryData, getDirFile } from "@/lib/apiUtils";
 import DiffPreview from '@/components/DiffPreview';
 import { Endpoints, OctokitResponse } from "@octokit/types";
 import { components } from "@octokit/openapi-types"
-
-
+import BoxPreview from "@/components/BoxPreview";
+import Filter from "@/components/HomeFilter";
+/*
 function compareDiff(a:any,b:any):number{
   const difficulties = ["Easy","Medium","Hard","Insane"];
   return difficulties.indexOf(a.name) - difficulties.indexOf(b.name);
-}
+}*/
 
-//type DirectoryItem = components["schemas"]["content-directory"];
+type DirectoryItem = components["schemas"]["content-directory"];
 
-export default async function HTB_Home({params}:any) {
-  
-  const difficulties = await getDirectoryData('HackTheBox');
-  difficulties.sort(compareDiff)
+export default async function HTB_Home() {
+  const r = await Promise.all([getDirectoryData('HackTheBox/Easy'),getDirectoryData('HackTheBox/Medium')])
+  const boxes = r.reduce((a,b)=> {return a.concat(b)})
 
-  return (
+  const boxProps = await Promise.all(boxes.map(async (box)=> {
+    const decodedContent = await getDirFile(box.path+`/${box.name}.txt`)
+
+    return {
+      name : box.name,
+      path : box.path,
+      sha : box.sha,
+      decodedContent : decodedContent
+    }
+  }))
+
+
+  return (  
+    
     <main>
       <PageWrapper>
-        <div className="flex flex-wrap w-[80vw] translate-x-[10vw] justify-center pt-[100px]">
-        {difficulties?.map((difficulty) => (
-            difficulties && <DiffPreview key={difficulty.sha} difficulty={difficulty.name} />
-          ))}
-        </div>
+      <Filter boxes={boxProps}/>
       </PageWrapper>
     </main>
+    
   );
 }
---
+++ components/HomeFilter.tsx
@@ -0,0 +1,113 @@
+'use client';
+import { getDirectoryData, getDirFile} from "@/lib/apiUtils";
+import HTB_Home from "@/app/HTB/page";
+import { ChangeEventHandler, SelectHTMLAttributes } from "react";
+import { useEffect,useState } from "react";
+import BoxPreview from "./BoxPreview";
+import { Endpoints, OctokitResponse } from "@octokit/types";
+import { components } from "@octokit/openapi-types"
+import Link from "next/link";
+import Image from "next/image";
+import { octokit } from "@/lib/octo";
+import { PageWrapper } from "./PageWrapper";
+
+const owner = 'Wanasgheo';
+const repo='Writeups';
+
+type BoxItemProps = {
+  name : string,
+  path : string,
+  sha : string
+  decodedContent : string
+}
+
+  type DirectoryItem = components["schemas"]["content-directory"];
+  export default function Filter({ boxes }: { boxes: { name: string; path: string; sha: string; decodedContent: string}[]; }) {
+  //console.log('b',boxes) 
+ 
+  const [difficulty, setDifficulty] : any = useState('all')
+   /*
+  const [boxes, setBoxes] = useState<DirectoryItem>()*/
+    
+    useEffect(()=>{
+      /*
+      const getData = () => {
+        Promise.all([getDirectoryData('HackTheBox/Easy'),getDirectoryData('HackTheBox/Medium')]).then(r => setBoxes(r.reduce((a,b)=> {return a.concat(b)})))
+      }
+      
+      if(!boxes){
+        getData()
+      }*/
+
+        window.addEventListener('click',function(e){
+            if(!document.getElementById('dropdown-button')?.contains(e.target as Node)){
+                document.getElementById('dropdown-menu')?.classList.add('hidden')
+            }
+        })
+        
+    },[])
+
+
+    
+    return(
+      <>
+      <PageWrapper>
+       <div className="relative">
+        <select onChange={e => setDifficulty(e.target.value)}>
+        <option value="all">All</option>
+          <option value="Easy">Easy</option>
+          <option value="Medium">Medium</option>
+          <option value="Hard">Hard</option>
+          <option value="Insane">Insane</option>
+        </select>
+        </div>
+      <div className="flex flex-wrap w-[80vw] translate-x-[10vw] justify-center pt-[100px]">
+        {boxes?.filter((box)=>{
+          if(difficulty !== 'all'){
+            return box.path.includes(difficulty)
+          }else{
+            return box.path.includes('HackTheBox')
+          }
+        }).map((box) =><Box name={box.name} difficulty={box.path.split("/")[1]} path={box.path} key={box.sha} decodedContent={box.decodedContent}/>)}
+        </div>
+        </PageWrapper>
+      </>
+
+    );      
+}
+
+const Box = ({name, difficulty,path, decodedContent}:any) =>{
+  
+  
+  return(
+
+    <div className="flex 
+                    m:w-[400px] m:h-[100px]
+                    xsm:w-[100px] xsm:h-[100px]
+                    m-2
+                    bg-white 
+                    justify-center rounded-[50px]"
+    >
+      <Link href={`/HTB/${difficulty}/${name}`}>
+        <div className=" relative w-[100px] h-full  ">
+        <Image className="rounded-[50px]" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' src={decodedContent}  alt=" "/>
+        </div>
+      </Link> 
+
+      
+      <div className="flex m:w-full xsm:w-0 justify-center items-center "> 
+        <center><h2 className="text-black xsm:invisible m:visible ">{name}</h2></center>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * <button className="py-2 px-4 bg-blue-500 text-white rounded shadow-md" id="dropdown-button" onClick={toggleDropdown}>
+          
+          menu a tendina  <i className="border-solid border-white border-r-2   border-b-2 display: inline-block p-[3px] transform: rotate-[45deg]" id="dropdown-icon"></i>
+        </button>
+        <ul className="absolute hidden mt-2 py-2 w-32 bg-white rounded shadow-md" id="dropdown-menu">
+          <li><a className="block px-4 py-2 hover:bg-gray-100"></a></li>
+        </ul>
+ */
