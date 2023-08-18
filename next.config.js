9e53104f4ae32a0d9c64dbcf6d3db705181d3dc3
unusualuser1 <cshater1234@gmail.com>
2023-08-18 12:02:47 +0000

    11

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
         // domains : ['github.com', ],
         domains: ['www.hackthebox.com','assets.tryhackme.com','github.com', 'lisandre.com'],
     },
-  output: 'export',
+    async rewrites(){
+        return[
+            {
+                source: '/gtag.js',
+                destination: 'https://www.googletagmanager.com/gtag/js?id=G-J8K4J5078W',
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig 
