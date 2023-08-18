902a96741afab6c1218f724b4ee4ae87729ab8b9
unusualuser1 <cshater1234@gmail.com>
2023-08-18 00:54:12 +0000

    4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:'export',
     images : {
         // domains : ['github.com', ],
         domains: ['www.hackthebox.com','assets.tryhackme.com','github.com', 'lisandre.com'],
