549bd0bf99d586a61d3fcc3ebb6d537c1a1048a2
unusualuser1 <cshater1234@gmail.com>
2023-08-19 10:48:07 +0000

    fixing

--- components/MDRenderer.tsx
+++ components/MDRenderer.tsx
@@ -1,137 +1,56 @@
-
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
 import gfm from "remark-gfm";
-import { atomOneDark, darcula, github, gruvboxDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { PageWrapper } from "@/components/PageWrapper";
 import rehypeRaw from "rehype-raw";
-import {unified} from 'unified'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import rehypeSanitize from 'rehype-sanitize'
-import rehypeStringify from 'rehype-stringify'
-import parse from 'remark-parse'
-import html from 'remark-html'
-import {remark} from 'remark'
-import rehypeHighlight from 'rehype-highlight'
-import remarkGfm from "remark-gfm";
-import rehypePrettyCode from 'rehype-pretty-code';
-import matter from "gray-matter";
-import remarkStringify from "remark-stringify";
-import { rehype } from "rehype";
-import 'highlight.js/styles/github-dark-dimmed.css';
 import React from "react";
-import { atomDark, oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { darkula } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import {
+  atomDark,
+  oneDark,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
 
-export default async function MDRenderer({decodedContent}: any){
-  
-  const matterResult = matter(decodedContent);
-
-  const processedContent = await unified()
-  .use(remarkParse)
-  .use(remarkGfm)
-  .use(remarkRehype,{allowDangerousHtml:true})
-  .use(rehypeHighlight,)
-  .use(rehypeStringify)
-  .use(rehypeRaw)
-  .process(matterResult.content);
+export default async function MDRenderer({ decodedContent }: any) {
   return (
     <>
-      <div className="md:px-[100px] md:pt-[100px] md:text-[20px]
+      <PageWrapper>
+        <div
+          className="md:px-[100px] md:pt-[100px] md:text-[20px]
                         sm:px-[50px]
                         xld:px-[450px]
                         ld:px-[250px]
                         xsm:text-[14px] xsm:px-[35px]
                         md: writeup
-                        text-white">
-          <ReactMarkdown
-  rehypePlugins={[rehypeRaw]}
-  remarkPlugins={[gfm]}
-  components={{
-    code({ node, inline, className, children, lang, ...props }) {
-      const match = /language-(\w+)/.exec(className || '');
-      return !inline && match ? (
-        <SyntaxHighlighter
-          {...props}
-          codeTagProps={{ style: { fontSize: "inherit" } }}
-          customStyle={{ fontSize: 18 }}
-          style={oneDark}
-          language={lang}
-          PreTag="div"
+                        text-white"
         >
-          {String(children).replace(/\n$/, '')}
-        </SyntaxHighlighter>
-      ) : (
-        <code className={className} {...props}>
-          {children}
-        </code>
-      );
-    },
-  }}
->
-  {decodedContent}
-</ReactMarkdown>
-          
-        </div>
-      
-    </>
-  );
-}
-
-/**
- * <PageWrapper>
-      <div dangerouslySetInnerHTML={{__html:processedContent.toString()}} className="md:px-[100px] md:pt-[100px] md:text-[20px]
-                        sm:px-[50px]
-                        xld:px-[450px]
-                        ld:px-[250px]
-                        xsm:text-[14px] xsm:px-[35px]
-                        md: writeup
-                        text-white"/>
-      </PageWrapper>
- */
-
-/**
- * const processedContent = await unified()
-  .use(remarkParse)
-  .use(remarkGfm)
-  .use(remarkRehype, { allowDangerousHtml: true })
-  .use(rehypePrettyCode, {
-    theme:'one-dark-pro',
-    keepBackground: false
-  })
-  .use(rehypeStringify)
-  .use(rehypeRaw)
-  .process(decodedContent);
- */
-
-/**
- *  <div className="md:px-[150px] md:py-[100px] md:text-[20px]
-                        xsm:text-[14px] xsm:px-[35px]
-                        md: writeup">
-        <ReactMarkdown
-        rehypePlugins={[rehypeRaw]}
-          remarkPlugins={[gfm]}
-            children={decodedContent}
+          <ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            remarkPlugins={[gfm]}
             components={{
-              code({node, inline, className, children, ...props}) {
-                const match = /language-(\w+)/.exec(className || '')
+              code({ node, inline, className, children, lang, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
                 return !inline && match ? (
                   <SyntaxHighlighter
                     {...props}
                     codeTagProps={{ style: { fontSize: "inherit" } }}
                     customStyle={{ fontSize: 18 }}
-                    children={String(children).replace(/\n$/, '')}
-                    style={atomOneDark}
-                    language={match[1]}
+                    style={oneDark}
+                    language={lang}
                     PreTag="div"
-                  />
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
                 ) : (
-                  <code {...props} className={className}>
+                  <code className={className} {...props}>
                     {children}
                   </code>
-                )
-              }
+                );
+              },
             }}
-          />
-        </div>*/
+          >
+            {decodedContent}
+          </ReactMarkdown>
+        </div>
+      </PageWrapper>
+    </>
+  );
+}
