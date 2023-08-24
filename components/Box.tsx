2b3b7090df3090ea5d0574c1cb83042b32bdb733
unusualuser1 <cshater1234@gmail.com>
2023-08-24 17:20:47 +0000

    fixing

--- components/Box.tsx
+++ components/Box.tsx
@@ -7,38 +7,44 @@ type BoxItemProps = {
   decodedContent : string
 }
 
+
 export const Box = ({name, path, decodedContent}:BoxItemProps) =>{
   const parsedData = JSON.parse(decodedContent);
   return(
 
-    <div className="flex xm:flex-wrap
-                    md:w-[650px] 
-                    xm:w-[250px] xm:h-[150px]
-                    xsm:w-[100px] xsm:h-[100px]
-                    m-2
-                    bg-[#3c3c3c] 
-                    justify-left rounded-[50px]
+    <div className="xm:w-full xm:h-[150px] xm:rounded-[30px]
+                    xsm:w-[100px] xsm:h-[100px] xsm:rounded-[50px]
+                    bg-[#3c3c3c] m-2
                     transform hover:scale-[0.95]
-                    transition-all"
+                    transition-all items-center xm:px-3
+                    inline-flex justify-center xm:space-x-4"
+
     >
-      <Link href={path} className="w-[100px] h-[100px] relative">
-        <div className=" w-full h-full m-7">
-          <Image className="rounded-[50px]" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' src={parsedData.link}  alt=" "/>
-        </div>
-      </Link> 
-
       
-      <div className="flex md:w-[200px] md:h-[30px] xmd:h-[100px] xm:w-[150px] xm:h-[30px] xsm:w-0 justify-center items-center "> 
-        <center><h2 className="text-white xsm:invisible md:visible  ">{name}</h2></center>
+      <div className=" w-[100px] h-[100px] relative">
+        <Link href={path} className="">
+          <Image className="rounded-[50px]" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' src={parsedData.link}  alt="box-image"/>
+        </Link>
       </div>
-
-      <div className="xsm:invisible xm:visible xsm:w-0 md:px-[50px] md:py-[20px] xm:px-[20px] xm:py-[5px] xm:w-full md:h-[100px] md:w-[100px] xm:h-[120px] flex items-center justify-center  text-white">
-      <ul className=" writeup">
-          <li>{parsedData.os}</li>
-          <li>{parsedData.difficulty}</li>
-          <li>{parsedData.release}</li>
-          <li>{parsedData.state}</li>
-        </ul>
+      <div className="xsm:w-0 xsm:space-x-0 xsm:invisible
+                      xm:space-x-2 xm:flex xm:flex-wrap xm:items-center xm:py-2 xm:w-[80%] xm:h-full xm:visible">
+        
+        <div className="flex justify-center items-center 
+                        xsm:w-0 xsm:invisible
+                        xm:visible xm:w-full xm:h-[30%]"
+        >
+        {name}
+        </div>
+        
+        <div className="filter
+                        xsm:w-0 xsm:invisible
+                        xm:visible xm:w-full xm:h-[70%] xm:flex xm:flex-wrap xm:justify-center xm:items-center"
+        >
+            <div>{parsedData.os}</div>
+            <div>{parsedData.difficulty}</div>
+            <div>{parsedData.release}</div>
+            <div>{parsedData.state}</div>
+        </div>
       </div>
     </div>
   )
