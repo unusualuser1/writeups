5999c689454033aeb64678700ae0cbe0ddfd745c
unusualuser1 <cshater1234@gmail.com>
2023-08-26 17:26:12 +0000

    fixing

--- components/backTopOfPage.tsx
+++ components/backTopOfPage.tsx
@@ -13,7 +13,7 @@ export default function TopOfPageButton(){
     return(
         <div className="w-[40px] h-[40px] bottom-0 right-0 absolute">
             <button onClick={scrollToTop} className="relative bg-white w-full h-full rounded-[100%]">
-                <Image src="/../frecciaSu.png" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' className=" translate-y-[2px] rotate-180" alt="moveUp"/>
+                <Image src="/frecciaSu.png" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' className=" translate-y-[2px] rotate-180" alt="moveUp"/>
             </button>
         </div>
     )
