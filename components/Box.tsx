cf812a53b7b5a6c52c6df1304969294da7717575
cala2313 <137945067+cala2313@users.noreply.github.com>
2023-11-08 09:48:08 +0100

    Update Box.tsx

--- components/Box.tsx
+++ components/Box.tsx
@@ -11,7 +11,7 @@ type BoxItemProps = {
 export const Box = ({name, path, decodedContent}:BoxItemProps) =>{
   const parsedData = JSON.parse(decodedContent);
   return(
-
+<Link href={path}>
     <div className="xm:w-full xm:h-[150px] xm:rounded-[30px]
                     xsm:w-[100px] xsm:h-[100px] xsm:rounded-[50px]
                     bg-[#3c3c3c] m-2
@@ -22,9 +22,9 @@ export const Box = ({name, path, decodedContent}:BoxItemProps) =>{
     >
       
       <div className=" w-[100px] h-[100px] relative">
-        <Link href={path}>
+        
           <Image className="rounded-[50px]" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' src={parsedData.link}  alt="box-image"/>
-        </Link>
+        
       </div>
       <div className="xsm:w-0 xsm:space-x-0 xsm:invisible
                       xm:space-x-2 xm:flex xm:flex-wrap xm:items-center xm:py-2 xm:w-[80%] xm:h-full xm:visible xm:justify-center">
@@ -48,5 +48,6 @@ export const Box = ({name, path, decodedContent}:BoxItemProps) =>{
         </div>
       </div>
     </div>
+  </Link>
   )
 }
