7138a9fb1938364c94bde18b5cc5505f5e9725d3
cala2313 <matte30sba@gmail.com>
2024-01-30 20:19:42 +0100

    fixed stuff

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
+import { useEffect, useState } from 'react';
 
 type BoxItemProps = {
   name: string,
@@ -8,12 +9,30 @@ type BoxItemProps = {
 }
 
 
+
 export const Box = ({ name, path, decodedContent }: BoxItemProps) => {
   const parsedData = JSON.parse(decodedContent);
 
-  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    import('mobile-detect').then(({ default: MobileDetect }) => {
+      const md = new MobileDetect(window.navigator.userAgent);
+      setIsMobile(!!md.mobile());
+
+      // Personalizza la logica in base alle tue esigenze
+      if (isMobile) {
+        document.body.classList.add('mobile-device');
+      } else {
+        document.body.classList.remove('mobile-device');
+      }
+    });
+  }, [isMobile]);
+
 
   return (
+
+
 
     <Link href={path} className="xm:w-full xm:h-[150px] xm:rounded-[30px]
     xsm:w-[100px] xsm:h-[100px] xsm:rounded-[50px]
@@ -21,10 +40,14 @@ export const Box = ({ name, path, decodedContent }: BoxItemProps) => {
     transform hover:scale-[0.95]
     transition-all items-center xm:px-3
     inline-flex justify-center xm:space-x-4 group">
-      
-        <div className=" w-[100px] h-[100px] relative">
-          <Image className="rounded-[50px]" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' src={parsedData.link} alt="box-image" />
-        </div>
+
+      <div className=" w-[100px] h-[100px] relative">
+        <Image className="rounded-[50px]" layout='fill' objectFit='contain' objectPosition="center" loading='lazy' src={parsedData.link} alt="box-image" />
+      </div>
+
+      {isMobile ? 
+        <></> 
+        :
         <div className="xsm:w-0 xsm:space-x-0 xsm:invisible
                       xm:space-x-2 xm:flex xm:flex-wrap xm:items-center xm:py-2 xm:w-[80%] xm:h-full xm:visible xm:justify-center">
 
@@ -45,7 +68,8 @@ export const Box = ({ name, path, decodedContent }: BoxItemProps) => {
             <div>{parsedData.release}</div>
             <div>{parsedData.state}</div>
           </div>
-        </div>
+        </div>}
+      
     </Link>
   )
 }
